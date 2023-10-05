b5e512d3a3645c8c1846da382966864f241dfa75
Mithravinda Nambiar <91652656+monganotmango@users.noreply.github.com>
2023-10-05 17:28:20 +0530

    commit

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -8,14 +8,88 @@ export const createUser = asyncHandler(async (req, res) => {
     let { email } = req.body;
     const userExists = await prisma.user.findUnique({ where: { email: email } })
     if (!userExists) {
-
         const user = await prisma.user.create({ data: req.body })
         res.send({
             message: "User registered successfully",
             user: user,
         });
-    };
-
-
-
+    } else res.status(201).send({ message: "User already registered" })
 });
+
+// function to book a visit to residency
+export const bookVisit = asyncHandler(async (req, res) => {
+    const { email, date } = req.body
+    const { id } = req.params
+
+    try {
+
+        const alreadyBooked = await prisma.user.findUnique({
+            where: { email },
+            select: { bookedVisits: true },
+        });
+
+        if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)) {
+            res
+                .status(400)
+                .json({ message: "This residency is already booked by you" });
+        } else {
+            await prisma.user.update({
+                where: { email: email },
+                data: {
+                    bookedVisits: { push: { id, date } },
+                },
+            });
+            res.send("Your visit is booked succesfully");
+        }
+
+    } catch (err) {
+        throw new Error(err.message);
+    }
+});
+
+// function to get all bookings of a user
+
+export const getAllBookings = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    try {
+        const bookings = await prisma.user.findUnique({
+            where: { email },
+            select: { bookedVisits: true }
+        })
+        res.status(200).send(bookings)
+    } catch (err) {
+        throw new Error(err.message);
+    }
+});
+
+// function to cancel a booking
+
+export const cancelBooking = asyncHandler(async (req, res) => {
+
+    const { email } = req.body;
+    const { id } = req.params
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+            select: { bookedVisits: true }
+        })
+        const index = user.bookedVisits.findIndex((visit) => visit.id === id)
+
+        if (index === -1) {
+            res.status(404).json({ message: "Booking not found" })
+        } else {
+            user.bookedVisits.splice(index, 1)
+            await prisma.user.update({
+                where: { email },
+                data: {
+                    bookedVisits: user.bookedVisits
+                }
+            })
+
+            res.send("Booking is cancelled successfully")
+        }
+    } catch (err) {
+        throw new Error(err.message);
+    }
+})
--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8000 || 3000;
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log('Server is running on port 8000');
 });
 
 app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
